fbd0962a4ed2ee5f2d147374c1487021775c0cec
ana <anabeatrizsouzaalves94@gmail.com>
2025-04-17 13:07:15 -0300

    condição if

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -25,3 +25,22 @@ frase.Trim('!');
 Console.WriteLine(frase.Trim('!'));
 
 
+const string APROVADO = "Aprovado!";
+const string REPROVADO = "Reprovado!";
+const string EM_RECUPERACAO = "Em Recuperação!";
+
+Console.WriteLine("Digite a nota  ");
+decimal nota = Convert.ToDecimal(Console.ReadLine());
+
+if (nota >= 5)
+{
+    Console.WriteLine(APROVADO);
+}
+else if(nota >= 4 && nota < 5)
+{
+    Console.WriteLine(EM_RECUPERACAO);
+}
+else
+{
+    Console.WriteLine(REPROVADO);
+}
